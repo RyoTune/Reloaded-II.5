4590053cf11489c6c53cd918f6f9d9a78f5f5d45
RyoTune <zutomayofan50@gmail.com>
2025-01-21 06:32:00 -0600

    Fallback to Github or Gamebanana update source for mod website, if available.

--- source/Reloaded.Mod.Launcher.Lib/Commands/Mod/VisitModProjectUrlCommand.cs
+++ source/Reloaded.Mod.Launcher.Lib/Commands/Mod/VisitModProjectUrlCommand.cs
@@ -1,10 +1,15 @@
-﻿namespace Reloaded.Mod.Launcher.Lib.Commands.Mod;
+﻿using Reloaded.Mod.Loader.Update.Providers.GitHub;
+
+namespace Reloaded.Mod.Launcher.Lib.Commands.Mod;
 
 /// <summary>
 /// A command that allows you to visit a website for a given mod.
 /// </summary>
 public class VisitModProjectUrlCommand : WithCanExecuteChanged, ICommand
 {
+    private static readonly GitHubReleasesUpdateResolverFactory _ghResolver = new();
+    private static readonly GameBananaUpdateResolverFactory _gbResolver = new();
+
     private readonly PathTuple<ModConfig>? _modTuple;
 
     /// <inheritdoc />
@@ -19,12 +24,35 @@ public class VisitModProjectUrlCommand : WithCanExecuteChanged, ICommand
         if (_modTuple == null)
             return false;
 
-        return !string.IsNullOrEmpty(_modTuple.Config.ProjectUrl);
+        return !string.IsNullOrEmpty(GetWebsiteUrl());
     }
 
     /// <inheritdoc />
     public void Execute(object? parameter)
     {
-        ProcessExtensions.OpenHyperlink(_modTuple!.Config.ProjectUrl);
+        ProcessExtensions.OpenHyperlink(GetWebsiteUrl()!);
+    }
+
+    private string? GetWebsiteUrl()
+    {
+        if (_modTuple == null)
+            return null;
+
+        if (!string.IsNullOrEmpty(_modTuple.Config.ProjectUrl))
+        {
+            return _modTuple.Config.ProjectUrl;
+        }
+
+        if (_ghResolver.TryGetConfiguration<GitHubReleasesUpdateResolverFactory.GitHubConfig>(_modTuple, out var ghConfig))
+        {
+            return $"https://www.github.com/{ghConfig.UserName}/{ghConfig.RepositoryName}";
+        }
+
+        if (_gbResolver.TryGetConfiguration<GameBananaUpdateResolverFactory.GameBananaConfig>(_modTuple, out var gbConfig))
+        {
+            return $"https://www.gamebanana.com/{gbConfig.ItemType.ToLowerInvariant()}s/{gbConfig.ItemId}";
+        }
+
+        return null;
     }
 }
