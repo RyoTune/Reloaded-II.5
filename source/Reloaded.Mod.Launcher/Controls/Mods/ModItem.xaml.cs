0ff36a7d96aa9033015771c75abfb6664162cdd0
RyoTune <zutomayofan50@gmail.com>
2025-01-25 20:37:03 -0600

    Add ModItem control.

---
+++ source/Reloaded.Mod.Launcher/Controls/Mods/ModItem.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Reloaded.Mod.Launcher.Controls.Mods;
+
+/// <summary>
+/// Interaction logic for ModItem.xaml
+/// </summary>
+public partial class ModItem : UserControl
+{
+    public ModItem()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty IsCompactProperty =
+        DependencyProperty.Register(
+            nameof(IsCompact),
+            typeof(bool),
+            typeof(ModItem),
+            new PropertyMetadata(false)
+        );
+
+    public bool IsCompact
+    {
+        get => (bool)GetValue(IsCompactProperty);
+        set => SetValue(IsCompactProperty, value);
+    }
+}
