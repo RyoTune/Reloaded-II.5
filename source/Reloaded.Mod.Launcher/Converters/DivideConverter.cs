30a721960544c28ce8af0206ebc1c56555d6d785
RyoTune <zutomayofan50@gmail.com>
2025-01-25 20:38:12 -0600

    Add converter for dividing.

---
+++ source/Reloaded.Mod.Launcher/Converters/DivideConverter.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Reloaded.Mod.Launcher.Converters;
+
+class DivideConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double d && double.TryParse((string)parameter, out var div))
+        {
+            return d / div;
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
