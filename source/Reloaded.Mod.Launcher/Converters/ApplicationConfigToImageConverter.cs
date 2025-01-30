20b15ae607f344431d82ddbd091f6344fa9bcad8
RyoTune <zutomayofan50@gmail.com>
2025-01-29 21:25:25 -0600

    Dump app icons and use highest resolution version.

--- source/Reloaded.Mod.Launcher/Converters/ApplicationConfigToImageConverter.cs
+++ source/Reloaded.Mod.Launcher/Converters/ApplicationConfigToImageConverter.cs
@@ -1,10 +1,10 @@
-using Imaging = System.Windows.Interop.Imaging;
-
 namespace Reloaded.Mod.Launcher.Converters;
 
 public class ApplicationPathTupleToImageConverter : IMultiValueConverter
 {
     public static ApplicationPathTupleToImageConverter Instance = new ApplicationPathTupleToImageConverter();
+
+    private readonly static Dictionary<string, IconFile[]> appIcons = [];
 
     /// <inheritdoc />
     public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
@@ -27,30 +27,78 @@ public class ApplicationPathTupleToImageConverter : IMultiValueConverter
     /// Obtains an image to represent a given application.
     /// The image is either a custom one or the icon of the application.
     /// </summary>
-    private ImageSource GetImageForAppConfig(PathTuple<ApplicationConfig> applicationConfig)
+    private ImageSource GetImageForAppConfig(PathTuple<ApplicationConfig> appConfig)
     {
         // Check if custom icon exists.
-        if (!string.IsNullOrEmpty(applicationConfig.Config.AppIcon))
+        if (!string.IsNullOrEmpty(appConfig.Config.AppIcon))
         {
-            if (ApplicationConfig.TryGetApplicationIcon(applicationConfig.Path, applicationConfig.Config, out var applicationIcon))
+            if (ApplicationConfig.TryGetApplicationIcon(appConfig.Path, appConfig.Config, out var applicationIcon))
                 return Misc.Imaging.BitmapFromUri(new Uri(applicationIcon, UriKind.Absolute));
         }
 
         // Otherwise extract new icon from executable.
-        var appLocation = ApplicationConfig.GetAbsoluteAppLocation(applicationConfig);
-        if (File.Exists(appLocation))
+        var appFile = ApplicationConfig.GetAbsoluteAppLocation(appConfig);
+        if (File.Exists(appFile))
         {
-            // Else make new from icon.
-            using Icon? ico = Icon.ExtractAssociatedIcon(appLocation);
-            if (ico != null)
+            var iconFile = GetAppIcon(appFile, appConfig);
+            if (iconFile != null)
             {
-                // Extract to config set location.
-                BitmapSource bitmapImage = Imaging.CreateBitmapSourceFromHIcon(ico.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-                bitmapImage.Freeze();
-                return bitmapImage;
+                var source = new BitmapImage(new(iconFile));
+                source.Freeze();
+                return source;
             }
         }
 
         return Misc.Imaging.GetPlaceholderIcon();
     }
+
+    private static string? GetAppIcon(string appFile, PathTuple<ApplicationConfig> appConfig)
+    {
+        if (appIcons.TryGetValue(appConfig.Config.AppId, out var icons))
+        {
+            return icons.FirstOrDefault()?.File;
+        }
+
+        var iconsDir = GetAppIconsDir(appConfig);
+        if (!Directory.EnumerateFiles(iconsDir, "*.png").Any())
+        {
+            DumpAppIcons(appFile, iconsDir);
+        }
+
+        var foundIcons = new List<IconFile>();
+        foreach (var file in Directory.EnumerateFiles(iconsDir, "*.png"))
+        {
+            if (int.TryParse(Path.GetFileNameWithoutExtension(file), out var size))
+            {
+                foundIcons.Add(new(file, size));
+            }
+        }
+
+        appIcons[appConfig.Config.AppId] = foundIcons.OrderByDescending(x => x.Size).ToArray();
+        return appIcons[appConfig.Config.AppId].FirstOrDefault()?.File;
+    }
+
+    private static void DumpAppIcons(string appFile, string iconsDir)
+    {
+        var iconRes = IconInfo.LoadIconsFromBinary(appFile);
+        foreach (var res in iconRes)
+        {
+            using var icon = Icon.FromHandle(res.Handle);
+            using var bitmap = icon.ToBitmap();
+
+            var outputFile = Path.Join(iconsDir, $"{icon.Width}.png");
+            bitmap.Save(outputFile);
+            res.Dispose();
+        }
+    }
+
+    private static string GetAppIconsDir(PathTuple<ApplicationConfig> appConfig)
+    {
+        var appDir = Path.GetDirectoryName(appConfig.Path);
+        var iconDir = Path.Join(appDir, "icons");
+        Directory.CreateDirectory(iconDir);
+        return iconDir;
+    }
+
+    private record IconFile(string File, int Size);
 }
