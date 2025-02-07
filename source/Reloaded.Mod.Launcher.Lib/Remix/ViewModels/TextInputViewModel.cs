6fdb37071980ba5eab2e57373877ba40ec002c65
RyoTune <zutomayofan50@gmail.com>
2025-02-07 17:22:45 -0600

    Add dialog prompt for text input.

---
+++ source/Reloaded.Mod.Launcher.Lib/Remix/ViewModels/TextInputViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Reloaded.Mod.Launcher.Lib.Remix.ViewModels;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public class TextInputViewModel
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Text { get; set; } = string.Empty;
+}
---
+++ source/Reloaded.Mod.Launcher/Controls/Dialogs/TextInputDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using Reloaded.Mod.Launcher.Lib.Remix.ViewModels;
+using System.Reactive.Disposables;
+
+namespace Reloaded.Mod.Launcher.Controls.Dialogs;
+
+/// <summary>
+/// Interaction logic for TextInputDialog.xaml
+/// </summary>
+public partial class TextInputDialog : ReactiveWindow<TextInputViewModel>
+{
+    public TextInputDialog()
+    {
+        InitializeComponent();
+
+        this.WhenActivated((CompositeDisposable disp) =>
+        {
+            this.DataContext = this.ViewModel;
+
+            RoutedEventHandler cancelHandler = (_, _) => { this.DialogResult = false; this.Close(); };
+            this.Button_Cancel.Click += cancelHandler;
+
+            RoutedEventHandler confirmHandler = (_, _) => { this.DialogResult = true; this.Close(); };
+            this.Button_Confirm.Click += confirmHandler;
+
+            Disposable.Create(() =>
+            {
+                this.Button_Cancel.Click -= cancelHandler;
+                this.Button_Confirm.Click -= confirmHandler;
+            })
+            .DisposeWith(disp);
+        });
+    }
+}
