f92f2d5de0ce5531c5e93b8000267bf641fd4579
RyoTune <zutomayofan50@gmail.com>
2025-02-08 10:17:14 -0600

    Add global interactions.

---
+++ source/Reloaded.Mod.Launcher.Lib/Remix/Interactions.cs
@@ -0,0 +1,10 @@
+﻿using ReactiveUI;
+using Reloaded.Mod.Launcher.Lib.Remix.ViewModels;
+
+namespace Reloaded.Mod.Launcher.Lib.Remix;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public static class Interactions
+{
+    public static readonly Interaction<TextInputViewModel, string?> PromptTextInput = new();
+}
