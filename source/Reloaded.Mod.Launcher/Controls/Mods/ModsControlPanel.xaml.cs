2e0e1b9167f25c7dff7ff1bcb844d92d3a83684f
RyoTune <zutomayofan50@gmail.com>
2025-02-08 10:14:15 -0600

    Add ModsControlPanel.

---
+++ source/Reloaded.Mod.Launcher/Controls/Mods/ModsControlPanel.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DynamicData;
+using DynamicData.Binding;
+using ReactiveUI;
+using Reloaded.Mod.Launcher.Lib.Remix.ViewModels;
+using System.Reactive.Disposables;
+
+namespace Reloaded.Mod.Launcher.Controls.Mods;
+
+/// <summary>
+/// Interaction logic for ModsControlPanel.xaml
+/// </summary>
+public partial class ModsControlPanel : ReactiveUserControl<ModsControlPanelViewModel>
+{
+    public ModsControlPanel()
+    {
+        InitializeComponent();
+
+        this.WhenActivated((CompositeDisposable disp) =>
+        {
+            this.ViewModel = this.DataContext as ModsControlPanelViewModel;
+
+            this.ViewModel!.Presets.ToObservableChangeSet()
+                .AutoRefresh()
+                .SkipInitial()
+                .Subscribe(_ =>
+                {
+                    this.ListView_Presets.Items.Refresh();
+                })
+                .DisposeWith(disp);
+
+            this.ViewModel!.WhenPropertyChanged(x => x.ShortcutsEnabled, false)
+                .Subscribe(_ => this.ListView_Presets.Items.Refresh())
+                .DisposeWith(disp);
+        });
+    }
+}
