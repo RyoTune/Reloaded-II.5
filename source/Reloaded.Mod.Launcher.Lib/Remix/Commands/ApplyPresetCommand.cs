2f2baee99975651901f98c7448e80044cfa1e743
RyoTune <zutomayofan50@gmail.com>
2025-02-08 10:18:05 -0600

    Add command for applying mods preset.

---
+++ source/Reloaded.Mod.Launcher.Lib/Remix/Commands/ApplyPresetCommand.cs
@@ -0,0 +1,35 @@
+﻿using Reloaded.Mod.Loader.IO.Remix.Mods;
+
+namespace Reloaded.Mod.Launcher.Lib.Remix.Commands;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public class ApplyPresetCommand : ICommand
+{
+    private readonly ConfigureModsViewModel _modsVm;
+    private readonly ModsPreset? _preset;
+
+    public ApplyPresetCommand(ConfigureModsViewModel modsVm, ModsPreset? preset = null)
+    {
+        _modsVm = modsVm;
+        _preset = preset;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter) => parameter as ModsPreset != null || _preset != null;
+
+    public void Execute(object? parameter)
+    {
+        var preset = parameter as ModsPreset ?? _preset;
+        if (preset != null && _modsVm.AllMods != null)
+        {
+            foreach (var mod in _modsVm.AllMods)
+            {
+                if (mod.IsEditable)
+                {
+                    mod.Enabled = preset.Mods.Any(x => x.Id == mod.Tuple.Config.ModId);
+                }
+            }
+        }
+    }
+}
