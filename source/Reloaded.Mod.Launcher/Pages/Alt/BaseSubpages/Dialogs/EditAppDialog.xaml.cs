3365941d430e6fa59348dac205d0d7db99224299
RyoTune <zutomayofan50@gmail.com>
2025-02-19 15:30:23 -0600

    Add new EditApp dialog.

---
+++ source/Reloaded.Mod.Launcher/Pages/Alt/BaseSubpages/Dialogs/EditAppDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using System.Reactive.Disposables;
+using EditAppViewModel = Reloaded.Mod.Launcher.Lib.Remix.ViewModels.EditAppViewModel;
+
+namespace Reloaded.Mod.Launcher.Pages.Alt.BaseSubpages.Dialogs;
+
+/// <summary>
+/// Interaction logic for EditAppDialog.xaml
+/// </summary>
+public partial class EditAppDialog : ReactiveWindow<EditAppViewModel>
+{
+    public EditAppDialog()
+    {
+        InitializeComponent();
+
+        this.WhenActivated((CompositeDisposable disp) =>
+        {
+            this.DataContext = this.ViewModel;
+
+            this.ViewModel!.DeleteAppCommand
+            .Subscribe(_ => this.Close())
+            .DisposeWith(disp);
+        });
+    }
+}
