2a1a29e0e33f04b69d4864a36155db774cbe1cd3
RyoTune <zutomayofan50@gmail.com>
2025-02-19 15:23:31 -0600

    AppImage convert support for both single and multi values.

--- source/Reloaded.Mod.Launcher/Converters/ApplicationPathTupleToImageConverter.cs
+++ source/Reloaded.Mod.Launcher/Converters/ApplicationPathTupleToImageConverter.cs
@@ -1,20 +1,31 @@
 namespace Reloaded.Mod.Launcher.Converters;
 
-public class ApplicationPathTupleToImageConverter : IValueConverter
+public class ApplicationPathTupleToImageConverter : IValueConverter, IMultiValueConverter
 {
     public static readonly ApplicationPathTupleToImageConverter Instance = new();
 
     /// <inheritdoc />
     public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => Convert([value], targetType, parameter, culture);
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        if (value is PathTuple<ApplicationConfig> config)
-            return ApplicationIcon.GetImageSource(config);
+        foreach (var value in values)
+        {
+            if (value is PathTuple<ApplicationConfig> config)
+                return ApplicationIcon.GetImageSource(config);
+        }
 
         return Imaging.BitmapFromUri(WpfConstants.PlaceholderImagePath);
     }
 
     /// <inheritdoc />
     public object? ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
     }
