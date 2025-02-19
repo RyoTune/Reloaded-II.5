d1a5b01325738a6f87667891107e1dd7c5af81ff
RyoTune <zutomayofan50@gmail.com>
2025-02-19 15:26:39 -0600

    Add command for opening items with the shell, such as folders.

---
+++ source/Reloaded.Mod.Launcher.Lib/Remix/Commands/OpenPathWithShellCommand.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Reloaded.Mod.Launcher.Lib.Remix.Commands;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public class OpenPathWithShellCommand : ICommand
+{
+    private readonly string? _path;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public OpenPathWithShellCommand()
+    {
+    }
+
+    public OpenPathWithShellCommand(string? path)
+    {
+        _path = path;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (Directory.Exists(_path) || File.Exists(_path))
+        {
+            return true;
+        }
+
+        if (parameter is string paramPath)
+        {
+            if (Directory.Exists(paramPath) || File.Exists(paramPath))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void Execute(object? parameter)
+    {
+        if (_path != null)
+        {
+            Process.Start(new ProcessStartInfo() { FileName = _path, UseShellExecute = true });
+        }
+
+        if (parameter is string paramPath)
+        {
+            Process.Start(new ProcessStartInfo() { FileName = paramPath, UseShellExecute = true });
+        }
+    }
+}
