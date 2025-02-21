6d63e0bce44ca3dcca2f8480d16f58c6b54ef567
RyoTune <zutomayofan50@gmail.com>
2025-02-21 06:57:23 -0600

    Add command for copying text to clipboard.

---
+++ source/Reloaded.Mod.Launcher.Lib/Remix/Commands/CopyToClipboardCommand.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Windows;
+
+namespace Reloaded.Mod.Launcher.Lib.Remix.Commands;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public class CopyToClipboardCommand : ICommand
+{
+    public static readonly CopyToClipboardCommand Instance = new();
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter)
+    {
+        if (parameter is string text)
+        {
+            Clipboard.SetText(text);
+        }
+    }
+}
