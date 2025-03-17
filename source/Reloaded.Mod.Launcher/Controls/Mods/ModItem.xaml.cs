7aeb63adef54a3e3c251acfa0342004d08ef8b39
RyoTune <zutomayofan50@gmail.com>
2025-03-17 00:05:07 -0500

    Display Author in ModItem when in compact mode. Add slight offset to separator header.

--- source/Reloaded.Mod.Launcher/Controls/Mods/ModItem.xaml.cs
+++ source/Reloaded.Mod.Launcher/Controls/Mods/ModItem.xaml.cs
@@ -62,4 +62,16 @@ public partial class ModItem : UserControl
         get => (bool)GetValue(IsCompactProperty);
         set => SetValue(IsCompactProperty, value);
     }
+
+    public Orientation Orientation
+    {
+        get { return (Orientation)GetValue(OrientationProperty); }
+        set { SetValue(OrientationProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Orientation.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty OrientationProperty =
+        DependencyProperty.Register("Orientation", typeof(Orientation), typeof(ModItem), new PropertyMetadata(Orientation.Vertical));
+
+
 }
