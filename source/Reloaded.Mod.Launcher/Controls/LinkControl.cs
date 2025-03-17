780baa49b372d02bc9ed1e933f7a45b6533f07cf
RyoTune <zutomayofan50@gmail.com>
2025-03-17 08:00:37 -0500

    Add generic Link control for opening paths, typically URLs.

---
+++ source/Reloaded.Mod.Launcher/Controls/LinkControl.cs
@@ -0,0 +1,39 @@
+﻿namespace Reloaded.Mod.Launcher.Controls;
+
+public class LinkControl : ContentControl
+{
+    protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+    {
+        e.Handled = true;
+
+        try
+        {
+            if (string.IsNullOrEmpty(Path)) return;
+
+            var procInfo = new ProcessStartInfo() { FileName = Path, UseShellExecute = true };
+            Process.Start(procInfo);
+        }
+        catch (Exception) { }
+    }
+
+    protected override void OnInitialized(EventArgs e)
+    {
+        base.OnInitialized(e);
+        this.ToolTip ??= Path;
+    }
+
+    static LinkControl()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(LinkControl), new FrameworkPropertyMetadata(typeof(LinkControl)));
+    }
+
+    public string? Path
+    {
+        get { return (string?)GetValue(PathProperty); }
+        set { SetValue(PathProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Path.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty PathProperty =
+        DependencyProperty.Register("Path", typeof(string), typeof(LinkControl), new PropertyMetadata(null));
+}
