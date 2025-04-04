9d4d950dc185c34e5342f2e6e419dd5c393d6cf7
RyoTune <zutomayofan50@gmail.com>
2025-04-04 00:49:31 -0500

    Add ReMIX Toolkit installer and CLI tool.

---
+++ source/RemixToolkit.HostMod.CLI/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Reloaded.Mod.Loader.IO.Config;
+using Reloaded.Mod.Loader.IO.Structs;
+using RemixToolkit.HostMod.Installer;
+using System.Diagnostics;
+
+var reloadedModsDir = Environment.GetEnvironmentVariable("RELOADEDIIMODS", EnvironmentVariableTarget.User);
+if (reloadedModsDir == null)
+{
+    Console.WriteLine("Failed to find Reloaded II mods folder.");
+    return;
+}
+
+var mods = Directory.EnumerateDirectories(reloadedModsDir)
+    .Select(x => Path.Join(x, ModConfig.ConfigFileName))
+    .Where(File.Exists)
+    .Select(x => new PathTuple<ModConfig>(x, IConfig<ModConfig>.FromPath(x)))
+    .OrderBy(x => x.Config.ModName)
+    .ToArray();
+
+for (int i = 0; i < mods.Length; i++)
+{
+    Console.WriteLine($"[ {i + 1} ] {mods[i].Config.ModName}");
+}
+
+int id;
+
+do
+{
+    Console.Write("\nAdd/Remove ReMIX Toolkit from Mod: ");
+}
+while (int.TryParse(Console.ReadLine(), out id) == false || id < 0 || id > mods.Length);
+
+id--;
+var selectedMod = mods[id];
+var modDir = Path.GetDirectoryName(selectedMod.Path)!;
+if (selectedMod.Config.ModDependencies.Contains(Installer.REMIX_MOD_ID))
+{
+    Console.WriteLine($"{selectedMod.Config.ModName}: Removing ReMIX Toolkit...");
+    Installer.Uninstall(modDir);
+    Console.WriteLine($"{selectedMod.Config.ModName}: ReMIX Toolkit Removed!");
+}
+else
+{
+    Console.WriteLine($"{selectedMod.Config.ModName}: Installing ReMIX Toolkit...");
+    Installer.Install(modDir);
+    Console.WriteLine($"{selectedMod.Config.ModName}: ReMIX Toolkit Installed!");
+}
+
+if (!Debugger.IsAttached) Console.ReadLine();
---
+++ source/RemixToolkit.HostMod.Installer/Installer.cs
@@ -0,0 +1,94 @@
+﻿using Reloaded.Mod.Loader.IO.Config;
+using Reloaded.Mod.Loader.Update.Providers;
+using Reloaded.Mod.Loader.Update.Providers.GitHub;
+using static Reloaded.Mod.Loader.Update.Providers.GitHub.GitHubReleasesUpdateResolverFactory;
+using Reloaded.Mod.Interfaces.Utilities;
+using SevenZip;
+
+namespace RemixToolkit.HostMod.Installer;
+
+public static class Installer
+{
+    public const string REMIX_MOD_ID = "RemixToolkit.Reloaded";
+    private const string REMIX_MOD_DLL = "RemixToolkit.HostMod.dll";
+    private static readonly string[] REMIX_MOD_FILES =
+    [
+        REMIX_MOD_DLL,
+        "YamlDotNet.dll",
+        "RemixToolkit.Core.Configs.dll",
+        "RemixToolkit.Core.Serializers.dll",
+        "RemixToolkit.Interfaces.dll",
+        "RemixToolkit.HostMod.deps.json",
+    ];
+
+    private static readonly DependencyResolverItem<GitHubConfig> REMIX_GITHUB_DEP = new()
+    {
+        Config = new()
+        {
+            RepositoryName = "RemixToolkit",
+            UseReleaseTag = false,
+            UserName = "RyoTune",
+        },
+
+        ReleaseMetadataName = "RemixToolkit.Reloaded.ReleaseMetadata.json",
+    };
+
+    public static void Install(string modDir)
+    {
+        var modConfigFile = Path.Join(modDir, ModConfig.ConfigFileName);
+        if (!File.Exists(modConfigFile)) return;
+
+        var mod = IConfig<ModConfig>.FromPathOrDefault(modConfigFile);
+
+        if (!mod.ModDependencies.Contains(REMIX_MOD_ID))
+        {
+            mod.ModDependencies = [REMIX_MOD_ID, ..mod.ModDependencies];
+
+            mod.PluginData.TryAdd(GitHubReleasesDependencyMetadataWriter.PluginId, new DependencyResolverMetadata<GitHubConfig>());
+            mod.PluginData.TryGetValue<DependencyResolverMetadata<GitHubConfig>>(GitHubReleasesDependencyMetadataWriter.PluginId, out var githubDeps);
+
+            if (!githubDeps.IdToConfigMap.ContainsKey(REMIX_MOD_ID))
+            {
+                githubDeps.IdToConfigMap[REMIX_MOD_ID] = REMIX_GITHUB_DEP;
+            }
+        }
+
+        mod.ModDll = REMIX_MOD_DLL;
+        var seven = new SevenZipExtractor(GetHostModPath());
+        seven.ExtractFiles(modDir, REMIX_MOD_FILES);
+
+        IConfig<ModConfig>.ToPath(mod, modConfigFile);
+    }
+
+    public static void Uninstall(string modDir)
+    {
+        var modConfigFile = Path.Join(modDir, ModConfig.ConfigFileName);
+        if (!File.Exists(modConfigFile)) return;
+
+        var mod = IConfig<ModConfig>.FromPathOrDefault(modConfigFile);
+        if (mod.ModDependencies.Contains(REMIX_MOD_ID))
+        {
+            mod.ModDependencies = mod.ModDependencies.Where(x => x != REMIX_MOD_ID).ToArray();
+
+            if (mod.PluginData.TryGetValue<DependencyResolverMetadata<GitHubConfig>>(GitHubReleasesDependencyMetadataWriter.PluginId, out var githubDeps))
+            {
+                githubDeps.IdToConfigMap.Remove(REMIX_MOD_ID);
+            }
+        }
+
+        mod.ModDll = string.Empty;
+
+        foreach (var fileName in REMIX_MOD_FILES)
+        {
+            File.Delete(Path.Join(modDir, fileName));
+        }
+
+        IConfig<ModConfig>.ToPath(mod, modConfigFile);
+    }
+
+    private static string GetHostModPath()
+    {
+        var hostModDir = Path.Join(Directory.GetCurrentDirectory(), "HostMod");
+        return Directory.EnumerateFiles(hostModDir, "RemixToolkit.HostMod*.7z").First();
+    }
+}
